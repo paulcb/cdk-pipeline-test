ef6bc0391ef5e410882a6c32f9d36d2dbe6c0deb
Paul Basinger <paulcb213@gmail.com>
2025-03-09 14:48:28 -0400

    update

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -12,5 +12,11 @@ export class LambdaStack extends cdk.Stack {
       handler: 'index.handler',
       code: new InlineCode('exports.handler = _ => "Hello, CDK test";')
     });
+
+    new Function(this, 'Foo' + envTag(Function.name), {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: new InlineCode('exports.handler = _ => "Hello, CDK test foo";')
+    });
   }
 }
